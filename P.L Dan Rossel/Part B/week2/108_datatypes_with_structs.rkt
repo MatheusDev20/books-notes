4ad02994c632111cc8fa43c84de124ef2302263c
MatheusDev20 <matheusdev20@gmail.com>
2023-03-11 13:47:06 -0300

    Part C files

--- P.L Dan Rossel/Part B/week2/108_datatypes_with_structs.rkt
+++ P.L Dan Rossel/Part B/week2/108_datatypes_with_structs.rkt
@@ -14,7 +14,8 @@
   (cond [(const? e) e] ; note returning an exp, not a number
         [(negate? e) (const (- (const-int (eval-exp (negate-e e)))))]
         [(add? e) (let ([v1 (const-int (eval-exp (add-e1 e)))]
-                        [v2 (const-int (eval-exp (add-e2 e)))])
+                        [v2 (const-int (eval-exp (add-e2 e)))]
+                        )
                     (const (+ v1 v2)))]
         [(multiply? e) (let ([v1 (const-int (eval-exp (multiply-e1 e)))]
                              [v2 (const-int (eval-exp (multiply-e2 e)))])
